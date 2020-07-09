64fc613f83d7885631be1dc42c11a06e3f7de902
Daniil Kuprianov <vaedermakar@mail.ru>
2020-07-09 23:27:10 +0300

    ue?

--- QtNutrition_components_assessment/main.cpp
+++ QtNutrition_components_assessment/main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    qDebug() << sizeof (long);
+
     return a.exec();
 }
